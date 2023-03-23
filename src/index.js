4e1de77f4e422ac61b57b9c2b184ee8ab8c43ce7
Michael Jeung <michaelj229@gmail.com>
2023-03-23 13:37:37 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function createToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  
+  btn.addEventListener('click', handleDelete)
+
+  p.textContent = `${todo} `
+  btn.textContent = 'x'
+  document.getElementById('tasks').appendChild(p)
+  p.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
